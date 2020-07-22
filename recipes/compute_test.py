a1a2030b2f333c95cf866fcd816bb73cd4084f9d
admin <dovz@vision.bi>
2020-07-22 23:42:54 +0300

    Deleted dataset TEST.dummy_dataset

--- recipes/compute_test.py
+++
@@ -1,9 +0,0 @@
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-import dataiku
-
-
-project_handle = dataiku.api_client().get_project(dataiku.default_project_key())
-vars = project_handle.get_variables()
-new_val = 'L_PARTKEY,L_LINENUMBER,L_QUANTITY'
-vars['standard']['column_list'] = new_val
-project_handle.set_variables(vars)
